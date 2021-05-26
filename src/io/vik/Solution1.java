8c8d1c5cbe9ecf279105b8c5efa59c5f2461c61e
VikNarasimhan <vikraman.narasimhan@yahoo.com>
2021-05-26 22:24:07 +1000

    solved first problem

---
+++ src/io/vik/Solution1.java
@@ -0,0 +1,41 @@
+package io.vik;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution1 {
+
+    public static void main(String[] args) {
+        System.out.println(findMatchInList(List.of(1, 2, 6, 9, 19), 23));
+        System.out.println(findMatchInListUsingMap(List.of(1, 2, 6, 9, 19), 23));
+    }
+
+    public static final boolean findMatchInList(List<Integer> integerList, int k) {
+
+        for (int num: integerList) {
+            if (hasInt(integerList, Math.abs(num - k))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean hasInt(List<Integer> integerList, int difference) {
+        return integerList.contains(difference) ? true : false;
+    }
+
+    public static final boolean findMatchInListUsingMap(List<Integer> integerList, int k) {
+        var mapWithDifferences = new HashMap<>();
+        for (var i: integerList) {
+            var difference = Math.abs(i - k);
+
+            if (difference == 0 || mapWithDifferences.containsKey(i)) {
+                return true;
+            } else {
+                mapWithDifferences.put(difference, 0);
+            }
+        }
+        return false;
+    }
+
+}
