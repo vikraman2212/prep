d0218ee678e18cf5e02f8f3b4b859e3ff9d81419
VikNarasimhan <vikraman.narasimhan@yahoo.com>
2021-06-01 23:22:40 +1000

    added the 2nd solution

--- src/io/vik/Solution2.java
+++ src/io/vik/Solution2.java
@@ -1,22 +1,26 @@
 package io.vik;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Solution2 {
     public static void main(String[] args) {
-        var arr = new int[] {1, 0, 3, 4, 0};
+        var arr = new int[] {1, 2, 0, 4, 0};
         var product = FindProductOfAnArray.getProduct(arr);
         var newArr = new int[arr.length];
         for (var i = 0; i < arr.length; i++) {
             if (arr[i] != 0) {
                 newArr[i] = product / arr[i];
             } else {
-                newArr[i] = 0;
+                newArr[i] = product;
             }
 
         }
 
+        var productWithoutDivision = FindProductOfAnArray.getProductWithoutDivision(arr);
+
         IntStream.range(0, newArr.length).forEach(i -> System.out.println(newArr[i]));
+        System.out.println(Arrays.toString(productWithoutDivision));
     }
 
     private static class FindProductOfAnArray {
@@ -31,6 +35,28 @@ public class Solution2 {
                 }
             }
             return product;
+        }
+
+        private static int[] getProductWithoutDivision(int[] arrayOfNumbers) {
+            var arrSize = arrayOfNumbers.length;
+            var newArr = new int[arrSize];
+            var product = 1;
+            for (var i = 0; i < arrSize; ++i ) {
+                newArr[i] = product;
+                if (arrayOfNumbers[i] != 0 ) {
+                    product = product * arrayOfNumbers[i];
+                }
+
+            }
+            var product1 = 1;
+            for (var i = arrSize-1; i >= 0 ; i--) {
+                newArr[i]*=product1;
+                if (arrayOfNumbers[i]!=0) {
+                    product1 *= arrayOfNumbers[i];
+                }
+
+            }
+            return newArr;
         }
 
     }
